ba5272afc91553beaba7401f18aa831a44a59137
getonow <brenda.delafuente@getonow.com>
2025-06-16 23:46:07 +0200

    Set BrowserRouter basename for GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -16,12 +16,12 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+<BrowserRouter basename="/beta1/">
+  <Routes>
+    <Route path="/" element={<Index />} />
+    <Route path="*" element={<NotFound />} />
+  </Routes>
+</BrowserRouter>
       </TooltipProvider>
     </QueryClientProvider>
   </ThemeProvider>
